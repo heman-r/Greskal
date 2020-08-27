ee5192beb06e361faaf0e14b6b4a306409cf7f9c
heman-r <ronycherian789@gamil.com>
2020-08-27 10:51:47 -0400

    #1-F Tic-tac-toe game

---
+++ face/build/precache-manifest.66d5177eec11d524a0f37ebceadefd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032cfbf8c0639a0470d21a4460d99f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09959323e726c02fe5b0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "391a73c7ec99a234f33d",
+    "url": "/static/js/2.d16c8304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d16c8304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09959323e726c02fe5b0",
+    "url": "/static/js/main.31a48474.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
---
+++ face/src/index.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+function Square(props){
+  
+    return (
+      <button 
+        className='square' 
+        onClick={()=> props.onClick()}
+      >
+          {props.value}
+      </button>
+    );
+}
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      winner: false
+    }
+  }
+
+  resetGame() {
+    let squares = Array(9).fill(null);
+    let xIsNext = true;
+    let winner = false;
+    this.setState({squares: squares, xIsNext: xIsNext, winner: winner})
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (!this.state.winner) {
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      
+      const winner = calculateWinner(squares);
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+        winner: winner
+      });
+    }
+  }
+
+  renderSquare(i) {
+    return <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />;
+  }
+
+  render() {
+    
+    let status = '';
+    if (this.state.winner) {
+      status = 'Winner : ' + this.state.winner;
+    } else {
+      status = 'Next Player:' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return(
+      <div>
+        <div className='Status'>
+          {status}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+
+        <div>
+          <button onClick={() => this.resetGame()}>
+            Reset Game
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+class Game extends React.Component {
+  // constructor(){
+  //   super(props);
+  //   this.state = {};
+  // }
+
+  renderBoard(params) {
+    return <Board />;
+  }
+
+  render(){
+    return (
+      <div className='game'>
+        <div className='board'>
+          <Board />
+        </div>
+      </div>
+      
+    )
+  }
+}
+
+ReactDOM.render(
+  // <React.StrictMode>
+  //   <App />
+    <Board />,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
