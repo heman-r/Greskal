dea73133fb0f43a56c5eeafd1a5717f2ca42c58c
heman-r <ronycherian789@gamil.com>
2020-08-27 13:39:29 -0400

    #3-S Deployment machine paths

--- server/inedx.js
+++ server/inedx.js
@@ -4,10 +4,10 @@ const path = require("path")
 const port = 3000;
 const app = express();
 
-app.use('/static', express.static(path.join("/home/neo/projects/Greskal/face/build/static")))
+app.use('/static', express.static(path.join("/home/ubuntu/Greskal/face/build/static")))
 
 app.get("/tic", (req, res) => {
-    res.sendFile(path.join("/home/neo/projects/Greskal/face", 'build', 'index.html'));
+    res.sendFile(path.join("/home/ubuntu/Greskal/face", 'build', 'index.html'));
 })
 
 app.get("/", (req, res)=>{
