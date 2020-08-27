4e0bfbb261034ea1e3d19af8ad9010f61fa0aa6c
heman-r <ronycherian789@gamil.com>
2020-08-27 13:53:20 -0400

    #4-B Update index file name

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 
-const port = 3000;
+const port = 80;
 const app = express();
 
 app.use('/static', express.static(path.join("/home/neo/projects/Greskal/face/build/static")))
