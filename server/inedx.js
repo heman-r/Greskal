8c6f9843111e37ba2d48cfc37f5d6fe4a84d017f
heman-r <ronycherian789@gamil.com>
2020-08-27 13:34:37 -0400

    #2-S Tic server

---
+++ server/inedx.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path = require("path")
+
+const port = 3000;
+const app = express();
+
+app.use('/static', express.static(path.join("/home/neo/projects/Greskal/face/build/static")))
+
+app.get("/tic", (req, res) => {
+    res.sendFile(path.join("/home/neo/projects/Greskal/face", 'build', 'index.html'));
+})
+
+app.get("/", (req, res)=>{
+    res.send("Hello world!");
+})
+
+app.listen(port, () => {
+    console.log("********** Server listening on Port 80 **********");
+})
+
