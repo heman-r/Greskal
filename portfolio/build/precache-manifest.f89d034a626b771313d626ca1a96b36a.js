08c2a8af5900488f3d8c98b2f9d016caec86c88c
heman-r <ronycherian789@gamil.com>
2020-10-01 16:55:59 -0400

    #9-P Portfolio react build

---
+++ portfolio/build/precache-manifest.f89d034a626b771313d626ca1a96b36a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e59332fb275e60c66527acd5114a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe12384489eb7f932401",
+    "url": "/static/css/main.51b357eb.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe12384489eb7f932401",
+    "url": "/static/js/main.229c9223.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c2d8f1311f2559684c679cbf7931aedd",
+    "url": "/static/media/t2.c2d8f131.jpg"
+  }
+]);
