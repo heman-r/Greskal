dacbbaf4242b411e41e9fa554c69c7cf614b185a
heman-r <ronycherian789@gamil.com>
2020-10-01 16:55:13 -0400

    #8-S Adding route portfolio

--- server/index.js
+++ server/index.js
@@ -4,10 +4,17 @@ const path = require("path")
 const port = 80;
 const app = express();
 
-app.use('/static', express.static(path.join("/home/ubuntu/Greskal/face/build/static")))
+app.use('/static', express.static(path.join("/home/ubuntu/Greskal/portfolio/build/static")))
 
 app.get("/tic", (req, res) => {
     res.sendFile(path.join("/home/ubuntu/Greskal/face", 'build', 'index.html'));
+})
+app.get("/portfolio", (req, res) => {
+    res.sendFile(path.join("/home/ubuntu/projects/Greskal/portfolio", 'build', 'index.html'));
+})
+
+app.get("/background-about-me.jpg", (req, res) => {
+    res.sendFile(path.join("/home/ubuntu/projects/Greskal/portfolio", 'build', 'background-about-me.jpg'));
 })
 
 app.get("/", (req, res)=>{
