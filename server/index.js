89c6f04335cb8d112fcf9e484fb98bce1fc6cda2
heman-r <ronycherian789@gamil.com>
2020-10-03 14:31:24 -0400

    16-S Creating https server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const path = require("path")
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 
 const port = 80;
 const app = express();
 
 app.use('/static', express.static(path.join("/home/ubuntu/Greskal/portfolio/build/static")))
+
+// app.get('*', function(req, res) {  
+//     res.redirect('https://' + req.headers.host + req.url);
+
+//     // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+//     // res.redirect('https://example.com' + req.url);
+// })
 
 app.get("/tic", (req, res) => {
     res.sendFile(path.join("/home/ubuntu/Greskal/face", 'build', 'index.html'));
@@ -21,7 +31,14 @@ app.get("/", (req, res)=>{
     res.send("Hello world!");
 })
 
-app.listen(port, () => {
-    console.log("********** Server listening on Port 80 **********");
-})
+https.createServer({
+    key: fs.readFileSync('/home/ubuntu/auth-cert/privkey.pem'),
+    cert: fs.readFileSync('/home/ubuntu/auth-cert/cert.pem')
+    },app).listen(3000, () => console.log('###Https server listening on port 443###'));
+
+// Redirect from http port 80 to https
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(3001);
 
